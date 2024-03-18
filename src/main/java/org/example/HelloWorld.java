fd99ddfbc24a8db6fb4daf06adeba8d39db4ab43
Erika Juliana Castro Romero <118181224+Juc28@users.noreply.github.com>
2024-03-17 19:13:01 -0500

    Update HelloWorld.java

--- src/main/java/org/example/HelloWorld.java
+++ src/main/java/org/example/HelloWorld.java
@@ -31,7 +31,7 @@ public class HelloWorld {
         if (System.getenv("PORT") != null) {
             return Integer.parseInt(System.getenv("PORT"));
         }
-        return 5000; //returns default port if heroku-port isn't set (i.e. on localhost)
+        return 5000; 
     }
 
 

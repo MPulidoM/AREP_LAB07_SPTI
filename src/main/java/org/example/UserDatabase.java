c6cc129291b41397e7e97d8c7fd873ce0e4e230f
Erika Juliana Castro Romero <118181224+Juc28@users.noreply.github.com>
2024-03-17 19:03:52 -0500

    Documentación

--- src/main/java/org/example/UserDatabase.java
+++ src/main/java/org/example/UserDatabase.java
@@ -7,9 +7,20 @@ import static spark.Spark.secure;
 import java.security.NoSuchAlgorithmException;
 import java.util.HashMap;
 import java.util.Map;
-
+/**
+ * Clase que representa un sistema de base de datos de usuarios.
+ */
 public class UserDatabase {
+    /**
+     * Mapa estático que almacena los usuarios y sus contraseñas cifradas.
+     */
     private static Map<String, String> db = new HashMap<>();
+    /**
+     * Método principal de la clase.
+     * @param args Argumentos de la línea de comandos.
+     * @throws NoSuchAlgorithmException Si ocurre un error al cifrar las contraseñas.
+
+     */
     public static void main(String[] args) throws NoSuchAlgorithmException {
 
         db.put("Juliana", PasswordCypher.cypherPassword("harryStyles08"));
@@ -28,7 +39,11 @@ public class UserDatabase {
             return PasswordCypher.validatePassword(password, db.get(username)) ? "Inicio de sesión exitosamente &#128151" : "Contraseña incorrecta &#x1F622";
         });
     }
-
+    
+     /**
+     * Método que devuelve el número de puerto en el que se ejecutará.
+     * @return Número de puerto.
+     */
     private static int getPort() {
         if (System.getenv("PORT") != null) {
             return Integer.parseInt(System.getenv("PORT"));
